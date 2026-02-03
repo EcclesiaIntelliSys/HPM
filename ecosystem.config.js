6a5291aa1f1165145fa13269595f6bc42597bc36
EcclesiaIntelliSys <ecclesia.intellisys@gmail.com>
2026-02-03 11:50:52 +0800

    changed deploy.yml

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
       env_production: {
         NODE_ENV: "production",
         PORT: 5000,
-        MONGO_URI: "mongodb+srv://admin:admin@cluster0.uwzkubf.mongodb.net/?dbname=HPS_DEV", 
+        MONGO_URI: "mongodb+srv://admin:admin@cluster0.uwzkubf.mongodb.net/HPS_DEV?retryWrites=true&w=majority", 
         REACT_APP_API_URL: "http://3.0.104.235:5000",
         JWT_SECRET: "quickbrownfox"
       }
