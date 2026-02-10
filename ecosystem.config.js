704869699d1a87b24ce3d6b7f2a0b507f7079371
EcclesiaIntelliSys <ecclesia.intellisys@gmail.com>
2026-02-11 00:17:39 +0800

    changes to deploy.yml and ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,10 +6,11 @@ module.exports = {
       env_production: {
         NODE_ENV: "production",
         PORT: 5000,
-        MONGO_URI: "mongodb+srv://admin:admin@cluster0.uwzkubf.mongodb.net/HPS_DEV?retryWrites=true&w=majority", 
-        REACT_APP_API_URL: "http://3.0.104.235:5000",
-        JWT_SECRET: "quickbrownfox"
-      }
-    }
-  ]
+        MONGO_URI:
+          "mongodb+srv://admin:admin@cluster0.uwzkubf.mongodb.net/HPS_DEV?retryWrites=true&w=majority",
+        REACT_APP_API_URL: "http://54.169.54.190/api",
+        JWT_SECRET: "quickbrownfox",
+      },
+    },
+  ],
 };
