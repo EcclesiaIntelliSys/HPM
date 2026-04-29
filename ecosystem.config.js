cca7ff9e24acc195fc3115b07be631d457f4b106
EcclesiaIntelliSys <ecclesia.intellisys@gmail.com>
2026-04-29 08:19:46 +0800

    GOLIVE 1.1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,26 +3,6 @@ module.exports = {
     {
       name: "hpm",
       script: "./server/server.js",
-      env_production: {
-        NODE_ENV: "production",
-        PORT: 5000,
-        MONGO_URI:
-          "mongodb+srv://admin:admin@cluster0.uwzkubf.mongodb.net/HPS_DEV?retryWrites=true&w=majority",
-        REACT_APP_API_URL: "https://heartprayermusic.com",
-        JWT_SECRET: "quickbrownfoxquickbrownfoxquickbrownfox",
-        REFRESH_SECRET: "xofnworbkciuqxofnworbkciuqxofnworbkciuq",
-        TITAN_USER: "a4eca4001@smtp-brevo.com",
-        TITAN_PASS: "bskI2f53TtZ9wFV",
-        TITAN_HOST: "smtp-relay.brevo.com",
-        TITAN_PORT: 587,
-        FRONTEND_URL: "https://heartprayermusic.com",
-        R2_BUCKET: "hpmobjects",
-        R2_ENDPOINT:
-          "https://44d2eac43dfd6e63fdb97596c6b99948.r2.cloudflarestorage.com",
-        R2_ACCESS_KEY: "1a22c5b9e0803eeed46e1381f7ae68ec",
-        R2_SECRET_KEY:
-          "3243ebdf040d0032e2a6149da6591c2cc330b24c357ef4452a5a06bc5e123789",
-      },
     },
   ],
 };
